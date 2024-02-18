3d7be206f9ef4f638110ef2d9c79e64d7b9aeb27
Probando73 <153937042+Probando73@users.noreply.github.com>
2024-02-18 10:37:11 -0300

    Update interfaz.py

--- interfaz.py
+++ interfaz.py
@@ -1,7 +1,42 @@
-from tkinter import *
+from tkinter import *  
+
+### Funciones ###
+
+# Ventanas secundarias 
+def ventana_secundaria(raiz, title):
+    nueva_ventana = Toplevel(raiz)
+    nueva_ventana.title(title)
+    nueva_ventana.config(width=500, height=500)
+  
+    return nueva_ventana
+    
+# Se crea ventana principal, titulo y tamaño
 
 ventana_principal = Tk()
 ventana_principal.title("Negocio 2024")
 ventana_principal.config(width=500, height=500)
+
+# Ventanas secundarias 
+
+ventana_clientes = ventana_secundaria(ventana_principal, 'Clientes')
+ventana_stocks = ventana_secundaria(ventana_principal, 'Stocks')
+ventana_pedidos = ventana_secundaria(ventana_principal, 'Pedidos')
+ventana_ventas = ventana_secundaria(ventana_principal, 'Ventas')
+
+# Labels (etiquetas de texto)
+
+label = Label(ventana_principal, 'Texto')
+
+# Botones
+
+clientes = Button(ventana_principal, text= 'Clientes', command= ventana_clientes)
+stocks = Button(ventana_principal, text= 'Stocks', command= ventana_stocks)
+pedidos = Button(ventana_principal, text= 'Pedidos', command= ventana_pedidos)
+ventas = Button(ventana_principal, text= 'Ventas', command= ventana_ventas)
+
+
+
+
+
 
 ventana_principal.mainloop()
