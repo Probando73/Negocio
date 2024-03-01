b0d9c15972763c786a9006e100b236005fb6370f
Probando73 <Probando.codigo73@gmail.com>
2024-03-01 20:07:19 -0300

    Se pasa todo a Clases, falta Ventas

--- interfaz.py
+++ interfaz.py
@@ -5,144 +5,187 @@ from tkinter import ttk
 
 ### Funciones ###
 
+
+class Ventana():
+    def __init__(self) -> None:
+        self.ventana_principal()
+
+    def ventana_principal(self,):
+        self.ventana_inicio = Tk()
+        self.ventana_inicio.title("Negocio 2024")
+        self.ventana_inicio.config(width=440, height=70)
+        self.ventana_inicio.resizable(0, 0)
+        self.botones()
+        self.ventana_inicio.grab_set()
+        self.ventana_inicio.mainloop()
+        return self.ventana_inicio
+
+    def botones(self,):
+        clientes = Button(self.ventana_inicio, text='Clientes', width=10, height=2, font=('bold', 12,),
+                          command=lambda: Ventana_clientes.ventana_clientes(self,)).place(x=5, y=10)
+        stocks = Button(self.ventana_inicio, text='Stocks', width=10, height=2, font=('bold', 12,),
+                        command=lambda: Ventana_Stocks.ventana_stocks(self,)).place(x=115, y=10)
+        pedidos = Button(self.ventana_inicio, text='Pedidos', width=10, height=2, font=('bold', 12,),
+                         command=lambda: Ventana_pedidos.ventana_pedidos(self,)).place(x=225, y=10)
+        ventas = Button(self.ventana_inicio, text='Ventas', width=10, height=2, font=('bold', 12,),
+                        command=lambda: self.ventana_ventas(self.ventana_principal)).place(x=335, y=10)
+
+
 # Ventanas secundarias #
 
+class Ventanas_secundarias(Ventana):
+    def __init__(self) -> None:
+        self.ventana_secundaria()
 
-def ventana_secundaria(raiz, title, ancho=500, alto=500):
-    """
-    Permite crear una ventana secundaria con parametros por default.
-    """
+    def ventana_secundaria(self, raiz, title, ancho=500, alto=500):
+        """
+        Permite crear una ventana secundaria con parametros por default.
+        """
 
-    nueva_ventana = Toplevel(raiz)
-    nueva_ventana.title(title)
-    nueva_ventana.config(width=ancho, height=alto)
-    nueva_ventana.resizable(0, 0)
-    nueva_ventana.grab_set()
-    return nueva_ventana
-
-
-def ventana_clientes():
-    """
-    Crea la ventana clientes con su respectiva interfaz.
-    """
-
-    ventana = ventana_secundaria(
-        ventana_principal, 'Clientes', ancho=472, alto=300)
-    Button(ventana, text='Nuevo', width=10, height=2, font=(
-        'bold', 10,), command=nuevo_cliente).place(x=10, y=10)
-    Button(ventana, text='Buscar', width=10, height=2, font=(
-        'bold', 10,), command=None).place(x=372, y=10)
-    Entry(ventana, textvariable=StringVar).place(x=230, y=30)
-    Label(ventana, text='Nombre :').place(x=170, y=30)
-    arbol = mostrar_treeview(ventana, 10, 60, *('Nombre', 'Telefono'))
-    return arbol
+        nueva_ventana = Toplevel(raiz)
+        nueva_ventana.title(title)
+        nueva_ventana.config(width=ancho, height=alto)
+        nueva_ventana.resizable(0, 0)
+        nueva_ventana.grab_set()
+        return nueva_ventana
 
 
-def nuevo_cliente():
-    """
-    Crea la interfaz para crear y guardar los datos de un nuevo cliente.
-    """
+class Ventana_clientes(Ventanas_secundarias):
 
-    ventana = ventana_secundaria(
-        ventana_principal, 'Nuevo cliente', ancho=200, alto=130)
-    Label(ventana, text='Nombre :').place(x=10, y=10)
-    Label(ventana, text='Telefono :').place(x=10, y=40)
-    text_nombre = StringVar()
-    text_telefono = IntVar()
-    caja_nombre = Entry(ventana, textvariable=text_nombre)
-    caja_nombre.place(x=70, y=10)
-    caja_telefono = Entry(ventana, textvariable=text_telefono)
-    caja_telefono.place(x=70, y=40)
+    def __init__(self) -> None:
+        self.ventana_clientes()
 
-    boton = Button(ventana, text='Guardar', width=10, height=2, font=(
-        'bold', 10,), command=guardar_cliente)
-    boton.place(x=60, y=70)
-    datos = [caja_nombre, caja_telefono.get()]
+    def ventana_clientes(self,):
+        """
+        Crea la ventana clientes con su respectiva interfaz.
+        """
 
-    return datos
+        ventana = Ventanas_secundarias.ventana_secundaria(self,
+                                                          self.ventana_inicio, title='Clientes', ancho=472, alto=300)
+        boton_nuevo = Button(ventana, text='Nuevo', width=10, height=2, font=(
+            'bold', 10,), command=lambda: Ventana_clientes.nuevo_cliente(self,))
+        boton_nuevo.place(x=10, y=10)
+        Button(ventana, text='Buscar', width=10, height=2, font=(
+            'bold', 10,), command=None).place(x=372, y=10)
+        Entry(ventana, textvariable=StringVar).place(x=230, y=30)
+        Label(ventana, text='Nombre :').place(x=170, y=30)
+        arbol = mostrar_treeview(ventana, 10, 60, *('Nombre', 'Telefono'))
+        return arbol
 
+    def nuevo_cliente(self, ):
+        """
+        Crea la interfaz para crear y guardar los datos de un nuevo cliente.
+        """
 
-def guardar_cliente():  # Falta la sintaxis para guardar en SQL
-    """
-    Guarda los datos nuevos en la tabla Clientes.
-    """
-    datos = ventana_clientes
-    insertar_datos_tree(datos)
+        ventana = Ventanas_secundarias.ventana_secundaria(
+            self, self.ventana_inicio, title='Nuevo cliente', ancho=200, alto=130)
+        Label(ventana, text='Nombre :').place(x=10, y=10)
+        Label(ventana, text='Telefono :').place(x=10, y=40)
+        text_nombre = StringVar()
+        text_telefono = IntVar()
+        caja_nombre = Entry(ventana, textvariable=text_nombre)
+        caja_nombre.place(x=70, y=10)
+        caja_telefono = Entry(ventana, textvariable=text_telefono)
+        caja_telefono.place(x=70, y=40)
 
+        boton = Button(ventana, text='Guardar', width=10, height=2, font=(
+            'bold', 10,), command=Ventana_clientes.guardar_cliente)
+        boton.place(x=60, y=70)
+        datos = [caja_nombre, caja_telefono.get()]
 
-def ventana_stocks():
-    """
-    Crea la ventana Stocks con su respectiva interfaz.
-    """
+        return datos
 
-    ventana = ventana_secundaria(ventana_principal, 'Stocks', 922, 300)
-    Button(ventana, text='Nuevo producto', width=14, height=2, font=(
-        'bold', 10,), command=nuevo_ingreso).place(x=10, y=10)
-    Button(ventana, text='Buscar', width=10, height=2, font=(
-        'bold', 10,), command=None).place(x=450, y=10)
-    Entry(ventana, textvariable=StringVar).place(x=310, y=30)
-    Label(ventana, text='Marca :').place(x=270, y=30)
-    treeview = mostrar_treeview(ventana, 10, 60, *('Marca', 'Producto',
-                                                   'Cantidad', 'Precio de mercado', 'Precio de venta'))
-    return ventana
-
-
-def nuevo_ingreso():
-    """
-    Crea la interfaz para crear y guardar los datos de un nuevo producto.
-    """
-
-    ventana = ventana_secundaria(ventana_principal, 'Nuevo producto', 250, 200)
-    labels = ('Marca', 'Producto', 'Cantidad',
-              'Precio de mercado', 'Precio de venta')
-    y = 10
-    y_e = 10
-    for x in labels:
-        Label(ventana, text=f'{x} :', justify=CENTER).place(x=10, y=y)
-        # ver de crear un diccionario con el tipo de dato
-        Entry(ventana, textvariable=StringVar).place(x=120, y=y_e)
-        y += 30
-        y_e += 30
-
-    Button(ventana, text='Guardar', width=10, height=1, font=(
-        'bold', 10,), command=None).place(x=90, y=160)
-    return ventana
+    def guardar_cliente(self,):  # Falta la sintaxis para guardar en SQL
+        """
+        Guarda los datos nuevos en la tabla Clientes.
+        """
+        datos = self.ventana_clientes
+        insertar_datos_tree(datos)
 
 
-def ventana_pedidos():
-    """
-    Crea la ventana Pedidos con su respectiva interfaz.
-    """
+class Ventana_Stocks(Ventana_clientes):
+    def __init__(self) -> None:
+        self.ventana_stocks()
 
-    ventana = ventana_secundaria(ventana_principal, 'Pedidos', 1222, 300)
-    Button(ventana, text='Nuevo', width=14, height=2, font=(
-        'bold', 10,), command=nuevo_pedido).place(x=10, y=10)
-    Button(ventana, text='Buscar', width=10, height=2, font=(
-        'bold', 10,), command=None).place(x=450, y=10)
-    Entry(ventana, textvariable=StringVar).place(x=310, y=30)
-    Label(ventana, text='Marca :').place(x=270, y=30)
-    treeview = mostrar_treeview(ventana, 10, 60, *('N° de pedido', 'Cliente', 'Marca', 'Producto',
-                                                   'Cantidad', 'Precio unitario', 'Subtotal'))
-    return ventana
+    def ventana_stocks(self,):
+        """
+        Crea la ventana Stocks con su respectiva interfaz.
+        """
+
+        ventana = Ventanas_secundarias.ventana_secundaria(
+            self, self.ventana_inicio, 'Stocks', 922, 300)
+        Button(ventana, text='Nuevo producto', width=14, height=2, font=(
+            'bold', 10,), command=lambda: Ventana_Stocks.nuevo_ingreso(self,)).place(x=10, y=10)
+        Button(ventana, text='Buscar', width=10, height=2, font=(
+            'bold', 10,), command=None).place(x=450, y=10)
+        Entry(ventana, textvariable=StringVar).place(x=310, y=30)
+        Label(ventana, text='Marca :').place(x=270, y=30)
+        treeview = mostrar_treeview(ventana, 10, 60, *('Marca', 'Producto',
+                                                       'Cantidad', 'Precio de mercado', 'Precio de venta'))
+        return ventana
+
+    def nuevo_ingreso(self,):
+        """
+        Crea la interfaz para crear y guardar los datos de un nuevo producto.
+        """
+
+        ventana = Ventanas_secundarias.ventana_secundaria(
+            self, self.ventana_inicio, 'Nuevo producto', 250, 200)
+        labels = ('Marca', 'Producto', 'Cantidad',
+                  'Precio de mercado', 'Precio de venta')
+        y = 10
+        y_e = 10
+        for x in labels:
+            Label(ventana, text=f'{x} :', justify=CENTER).place(x=10, y=y)
+            # ver de crear un diccionario con el tipo de dato
+            Entry(ventana, textvariable=StringVar).place(x=120, y=y_e)
+            y += 30
+            y_e += 30
+
+        Button(ventana, text='Guardar', width=10, height=1, font=(
+            'bold', 10,), command=None).place(x=90, y=160)
+        return ventana
 
 
-def nuevo_pedido():
-    """
-    Crea la interfaz para crear y guardar los datos de un nuevo pedido.
-    """
+class Ventana_pedidos(Ventana_Stocks):
+    def __init__(self) -> None:
+        self.ventana_pedidos()
 
-    ventana = ventana_secundaria(ventana_principal, 'Nuevo pedido')
-    cuadro = Frame(ventana, width=430, height=350,
-                   bg='blue', relief='sunken').place(x=20, y=100)
-    # probar ahora si coloca el label en el frame
-    Label(cuadro, text='probando').place(x=30, y=140)
-    # treeview = mostrar_treeview(cuadro, 10, 60, *('N° de pedido', 'Cliente', 'Marca', 'Producto',
-    #                                              'Cantidad', 'Precio unitario', 'Subtotal'))
+    def ventana_pedidos(self,):
+        """
+        Crea la ventana Pedidos con su respectiva interfaz.
+        """
 
-    return ventana
+        ventana = Ventanas_secundarias.ventana_secundaria(
+            self, self.ventana_inicio, 'Pedidos', 1222, 300)
+        Button(ventana, text='Nuevo', width=14, height=2, font=(
+            'bold', 10,), command=lambda: Ventana_pedidos.nuevo_pedido(self,)).place(x=10, y=10)
+        Button(ventana, text='Buscar', width=10, height=2, font=(
+            'bold', 10,), command=None).place(x=450, y=10)
+        Entry(ventana, textvariable=StringVar).place(x=310, y=30)
+        Label(ventana, text='Marca :').place(x=270, y=30)
+        treeview = mostrar_treeview(ventana, 10, 60, *('N° de pedido', 'Cliente', 'Marca', 'Producto',
+                                                       'Cantidad', 'Precio unitario', 'Subtotal'))
+        return ventana
+
+    def nuevo_pedido(self,):
+        """
+        Crea la interfaz para crear y guardar los datos de un nuevo pedido.
+        """
+
+        ventana = Ventanas_secundarias.ventana_secundaria(
+            self, self.ventana_inicio, title='Nuevo pedido')
+        cuadro = Frame(ventana, width=430, height=350,
+                       bg='blue', relief='sunken').place(x=20, y=100)
+        # probar ahora si coloca el label en el frame
+        Label(cuadro, text='probando').place(x=30, y=140)
+        # treeview = mostrar_treeview(cuadro, 10, 60, *('N° de pedido', 'Cliente', 'Marca', 'Producto',
+        #                                              'Cantidad', 'Precio unitario', 'Subtotal'))
+
+        return ventana
 
 
-def ventana_ventas():
+def ventana_ventas(ventana_principal):
     """
     Crea la ventana Ventas con su respectiva interfaz.
     """
@@ -198,65 +241,3 @@ def insertar_datos_tree(tree):
     """
 
     tree.insert('', END, values=['aaa', 111])
-
-################## Se crea ventana principal, titulo y tamaño ##################
-
-
-ventana_principal = Tk()
-ventana_principal.title("Negocio 2024")
-ventana_principal.config(width=440, height=70)
-ventana_principal.resizable(0, 0)
-
-################## Ventanas secundarias ##################
-
-
-# Pedidos #
-"""
-Nueva ventana con 3 botones 
-"""
-# nuevo_pedido = ventana_secundaria(
-#    ventana_principal, 'Nuevo pedido')  # verificar si responde a la raiz o al toplevel.
-# buscar_pedido = ventana_secundaria(ventana_principal, 'Buscar pedido')
-# editar_pedido = ventana_secundaria(ventana_principal, 'Editar pedido')
-
-# Ventas #
-"""
-Nueva ventana con un listado de ventas y 1 boton 
-"""
-# buscar_venta = ventana_secundaria(ventana_principal, 'Buscar ventas')
-
-
-################## Botones ventana principal ##################
-
-## Determinar posicionamiento ##
-# Botones principales
-
-clientes = Button(ventana_principal, text='Clientes', width=10, height=2, font=('bold', 12,),
-                  command=ventana_clientes).place(x=5, y=10)
-stocks = Button(ventana_principal, text='Stocks', width=10, height=2, font=('bold', 12,),
-                command=ventana_stocks).place(x=115, y=10)
-pedidos = Button(ventana_principal, text='Pedidos', width=10, height=2, font=('bold', 12,),
-                 command=ventana_pedidos).place(x=225, y=10)
-ventas = Button(ventana_principal, text='Ventas', width=10, height=2, font=('bold', 12,),
-                command=ventana_ventas).place(x=335, y=10)
-
-
-# Botones ventana Stocks #
-
-# stocks_nuevo = Button(ventana_principal, text='Nuevo', command=ingreso_stocks)
-# stocks_editar = Button(ventana_principal, text='Editar', command=editar_stocks)
-# stocks_buscar = Button(ventana_principal, text='Buscar', command=buscar_stocks)
-
-# Botones ventana Pedidos #
-
-# pedidos_nuevo = Button(ventana_principal, text='Nuevo', command=nuevo_pedido)
-# pedidos_editar = Button(
-#    ventana_principal, text='Editar', command=editar_pedido)
-# pedidos_buscar = Button(
-#    ventana_principal, text='Buscar', command=buscar_pedido)
-
-# Botones ventana Ventas #
-
-# ventas_buscar = Button(ventana_principal, text='Nuevo', command=buscar_venta)
-ventana_principal.grab_set()
-ventana_principal.mainloop()
--- main.py
+++ main.py
@@ -1,89 +1,145 @@
 import clase_bd
+import interfaz as gui
+from tkinter import *
+from tkinter import ttk
 
 if __name__ == "__main__":
 
-  # Diccionarios con los nombres de las tablas y tipo de campo.
-  tabla_clientes = {
-      'ID': 'INTEGER  AUTO_INCREMENT PRIMARY KEY',
-      'Cliente': 'TEXT NOT NULL',
-      'Telefono': 'INTEGER NOT NULL',
-  }
+    # Diccionarios con los nombres de las tablas y tipo de campo.
+    tabla_clientes = {
+        'ID': 'INTEGER  AUTO_INCREMENT PRIMARY KEY',
+        'Cliente': 'TEXT NOT NULL',
+        'Telefono': 'INTEGER NOT NULL',
+    }
 
-  tabla_ventas = {
-      'N° Pedido': 'INTEGER AUTO_INCREMENT PRIMARY KEY',
-      'Cliente': 'TEXT NOT NULL ',
-      'Cant_Productos': 'INTEGER NOT NULL',
-      'Monto': 'INTEGER NOT NULL',
-  }
+    tabla_ventas = {
+        'N° Pedido': 'INTEGER AUTO_INCREMENT PRIMARY KEY',
+        'Cliente': 'TEXT NOT NULL ',
+        'Cant_Productos': 'INTEGER NOT NULL',
+        'Monto': 'INTEGER NOT NULL',
+    }
 
-  tabla_pedidos = {
-      'ID': 'INTEGER AUTO_INCREMENT ',
-      'N° Pedido': 'INTEGER PRIMARY KEY',
-      'Cliente': 'TEXT NOT NULL',
-      'Marca': 'TEXT NOT NULL',
-      'Producto': 'TEXT NOT NULL',
-      'Cantidad': 'INTEGER NOT NULL',
-      'Precio_unitario': 'INTEGER NOT NULL',
-      'Subtotal': 'INTEGER NOT NULL',
-  }
+    tabla_pedidos = {
+        'ID': 'INTEGER AUTO_INCREMENT ',
+        'N° Pedido': 'INTEGER PRIMARY KEY',
+        'Cliente': 'TEXT NOT NULL',
+        'Marca': 'TEXT NOT NULL',
+        'Producto': 'TEXT NOT NULL',
+        'Cantidad': 'INTEGER NOT NULL',
+        'Precio_unitario': 'INTEGER NOT NULL',
+        'Subtotal': 'INTEGER NOT NULL',
+    }
 
-  tabla_stocks = {
-      'ID': 'INTEGER  AUTO_INCREMENT',
-      'Marca': 'TEXT NOT NULL',
-      'Producto': 'TEXT  NOT NULL PRIMARY KEY',
-      'Cantidad': 'INTEGER NOT NULL',
-      'Precio_mercado': 'INTEGER NOT NULL',
-      'Precio_de_venta': 'INTEGER NOT NULL',
-  }
+    tabla_stocks = {
+        'ID': 'INTEGER  AUTO_INCREMENT',
+        'Marca': 'TEXT NOT NULL',
+        'Producto': 'TEXT  NOT NULL PRIMARY KEY',
+        'Cantidad': 'INTEGER NOT NULL',
+        'Precio_mercado': 'INTEGER NOT NULL',
+        'Precio_de_venta': 'INTEGER NOT NULL',
+    }
+"""
+    # Se crea la base de datos principal.
+    base_datos = clase_bd.BBDD("Negocio_2024")
 
-  # Se crea la base de datos principal.
-  base_datos = clase_bd.BBDD("Negocio_2024")
+    # Se crean las tablas en la BBDD
+    clientes = base_datos.create_table("clientes", **tabla_clientes)
 
-  # Se crean las tablas en la BBDD
-  clientes = base_datos.create_table("clientes", **tabla_clientes)
+    ventas = base_datos.create_table("ventas",
+                                     Fk='Cliente',
+                                     References_table='clientes',
+                                     references_field='ID',
+                                     **tabla_ventas)
 
-  ventas = base_datos.create_table("ventas",
-                                   Fk='Cliente',
-                                   References_table='clientes',
-                                   references_field='ID',
-                                   **tabla_ventas)
+    pedidos = base_datos.create_table("pedidos",
+                                      Fk='N° Pedido',
+                                      References_table='ventas',
+                                      references_field='N° Pedido',
+                                      **tabla_pedidos)
 
-  pedidos = base_datos.create_table("pedidos",
-                                    Fk='N° Pedido',
-                                    References_table='ventas',
-                                    references_field='N° Pedido',
-                                    **tabla_pedidos)
+    stocks = base_datos.create_table("stocks",
+                                     Fk='Precio_de_venta',
+                                     References_table='pedidos',
+                                     references_field='Precio_unitario',
+                                     **tabla_stocks)
 
-  stocks = base_datos.create_table("stocks",
-                                   Fk='Precio_de_venta',
-                                   References_table='pedidos',
-                                   references_field='Precio_unitario',
-                                   **tabla_stocks)
+    # Se crean los registros a guardar
 
-  # Se crean los registros a guardar
+    datos_clientes = [
+        ('Maximiliano', 1156236548),
+        ('Juan', 1156243548),
+        ('Maria', 1156243548),
+        ('Jose', 1156243548),
+    ]
 
-  datos_clientes = [
-      ('Maximiliano', 1156236548),
-      ('Juan', 1156243548),
-      ('Maria', 1156243548),
-      ('Jose', 1156243548),
-  ]
+    datos_stocks = [
+        ('Sedal', 'Acondicionador 650 ml', 24, 1300, 2000),
+        ('Sedal', 'Acondicionador 950 ml', 24, 1300, 2000),
+        ('Sedal', 'Acondicionador 1250 ml', 24, 1300, 2000),
+        ('Sedal', 'Acondicionador 1500 ml', 24, 1300, 2000),
+    ]
 
-  datos_stocks = [
-      ('Sedal', 'Acondicionador 650 ml', 24, 1300, 2000),
-      ('Sedal', 'Acondicionador 950 ml', 24, 1300, 2000),
-      ('Sedal', 'Acondicionador 1250 ml', 24, 1300, 2000),
-      ('Sedal', 'Acondicionador 1500 ml', 24, 1300, 2000),
-  ]
+    # Inserto los datos creados en la tabla clientes
 
-  # Inserto los datos creados en la tabla clientes
+    base_datos.insert_more('clientes', *datos_clientes)
+    base_datos.insert_more('stocks', *datos_stocks)
+    print(base_datos.read('clientes'))
 
-  base_datos.insert_more('clientes', *datos_clientes)
-  base_datos.insert_more('stocks', *datos_stocks)
-  print(base_datos.read('clientes'))
+    base_datos.update_where('clientes', 'Telefono', 800,
+                            'Cliente', '=', 'Maximiliano')
+    print(base_datos.read('clientes'))
 
-  base_datos.update_where('clientes', 'Telefono', 800, 'Cliente', '=','Maximiliano')
-  print(base_datos.read('clientes'))
+    # base_datos.delete_where('clientes', 'Cliente', '=', 'Juan')
+    print(base_datos.read('clientes'))
 
-  #base_datos.delete_where('clientes', 'Cliente', '=', 'Juan')
-  print(base_datos.read('clientes'))
+    ################## Se crea ventana principal, titulo y tamaño ##################
+
+
+################## Ventanas secundarias ##################
+
+
+# Pedidos #
+"""
+# Nueva ventana con 3 botones
+"""
+# nuevo_pedido = ventana_secundaria(
+#    ventana_principal, 'Nuevo pedido')  # verificar si responde a la raiz o al toplevel.
+# buscar_pedido = ventana_secundaria(ventana_principal, 'Buscar pedido')
+# editar_pedido = ventana_secundaria(ventana_principal, 'Editar pedido')
+
+# Ventas #
+"""
+# Nueva ventana con un listado de ventas y 1 boton
+"""
+# buscar_venta = ventana_secundaria(ventana_principal, 'Buscar ventas')
+
+#### instancio la clase ventana ###
+
+"""
+ventana_principal = gui.Ventana()
+"""
+################## Botones ventana principal ##################
+
+## Determinar posicionamiento ##
+# Botones principales
+
+
+# Botones ventana Stocks #
+
+# stocks_nuevo = Button(ventana_principal, text='Nuevo', command=ingreso_stocks)
+# stocks_editar = Button(ventana_principal, text='Editar', command=editar_stocks)
+# stocks_buscar = Button(ventana_principal, text='Buscar', command=buscar_stocks)
+
+# Botones ventana Pedidos #
+
+# pedidos_nuevo = Button(ventana_principal, text='Nuevo', command=nuevo_pedido)
+# pedidos_editar = Button(
+#    ventana_principal, text='Editar', command=editar_pedido)
+# pedidos_buscar = Button(
+#    ventana_principal, text='Buscar', command=buscar_pedido)
+
+# Botones ventana Ventas #
+
+# ventas_buscar = Button(ventana_principal, text='Nuevo', command=buscar_venta)
+
+"""
